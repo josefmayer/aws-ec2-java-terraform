c7195814f25f57908caf91fb8925c7fc4b8ec761
Josef Mayer <mjosef.it@gmx.net>
2017-05-05 15:24:26 +0200

    upload and run a java program

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
-## AWS EC2 Terraform 
+## AWS EC2 Java Terraform 
 Create a security group and a EC2 instance on Amazon AWS cloud with Terraform<br />
 Create ssh connectivity, move public key to server <br />
 Connect via ssh <br />
-Install software on server <br />
+Install JVM on server with provisioner 'remote-exec' <br />
+Copy Java code to server with provisioner 'file' <br />
+Run Java program <br />
 
 
 ### Technologies
-Terraform, AWS, SSH
+Terraform, AWS, SSH, Java
 
 
 ### Steps
--- main.tf
+++ main.tf
@@ -8,8 +8,8 @@ variable public_key_path{
   default = "~/.ssh/key_pair_aws_1_id_rsa.pub"
 }
 
-resource "aws_key_pair" "aws_1" {
-  key_name   = "aws_1"               # key pair name AWS
+resource "aws_key_pair" "aws_17" {
+  key_name   = "aws_17"               # key pair name AWS
   public_key = "${file(var.public_key_path)}"
 }
 
@@ -21,8 +21,8 @@ provider "aws" {
 #  cidr_block = "10.0.0.0/16"
 #}
 
-resource "aws_security_group" "terraform_14" {
-  name        = "terraform_14"
+resource "aws_security_group" "terraform_17" {
+  name        = "terraform_17"
   description = "Used in the terraform"
   # vpc_id      = "${aws_vpc.default.id}"
 
@@ -52,13 +52,13 @@ resource "aws_security_group" "terraform_14" {
 }
 
 
-resource "aws_instance" "ec2_instance_14" {
+resource "aws_instance" "ec2_instance_17" {
   ami = "ami-060cde69"
   instance_type = "t2.micro"
 
-  key_name = "${aws_key_pair.aws_1.id}"
+  key_name = "${aws_key_pair.aws_17.id}"
 
-  vpc_security_group_ids = ["${aws_security_group.terraform_14.id}"]
+  vpc_security_group_ids = ["${aws_security_group.terraform_17.id}"]
 
 
   connection {
@@ -69,14 +69,34 @@ resource "aws_instance" "ec2_instance_14" {
     # The connection will use the local SSH agent for authentication.
   }
 
+  # install java, create dir
   provisioner "remote-exec" {
     inline = [
       "sudo apt-get -y update",
       "sudo apt-get -y install openjdk-8-jre-headless",
-      #"sudo apt-get -y install openjdk-8-jre",
-      # "sudo apt-get -y install nginx",
-      # "sudo service nginx start",
-      # "sudo mkdir ~/mynewdir",
+      "mkdir data",
+      "cd data",
+      "mkdir inbox",
+      "cd ..",
+    ]
+  }
+
+  # upload jar file
+  provisioner "file" {
+    source      = "/mnt/c/Java/Sources/sources_aws/aws-io-file/target/io-file-copy-1.0-SNAPSHOT.jar"
+    destination = "/home/ubuntu/io-file-copy-1.0-SNAPSHOT.jar"
+  }
+
+  # upload inbox directory content
+  provisioner "file" {
+    source      = "/mnt/c/Java/Sources/sources_aws/aws-io-file/data/inbox/"
+    destination = "/home/ubuntu/data/inbox"
+  }
+
+  # run jar
+  provisioner "remote-exec" {
+    inline = [
+      "java -jar io-file-copy-1.0-SNAPSHOT.jar",
     ]
   }
 
